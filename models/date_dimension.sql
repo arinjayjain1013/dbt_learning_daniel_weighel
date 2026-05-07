37deb54158d7e1b961cb5c42c7c29f4dbe501239
arinjay jain <arinjay.m.jain@kipi.ai>
2026-05-07 17:05:41 +0000

    Added date_input_weather_output macro, daily_weather, model

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -12,7 +12,8 @@ with date_ranges as
     case when month(starttime) in (12,1,2) then 'Winter'
     when month(starttime) in (3,4,5,6,9,10,11) then 'Summer'
     else 'Rainy'
-    end as weather_started_at
+    end as weather_started_at, 
+    {{ date_to_weather('starttime')}} as weather_started_at_using_macro
     
     from 
     {{ source('sf_trial_5_may', 'bike_data') }}
