e42ca60295390522af9af5591fc825de565c6ce5
arinjay jain <arinjay.m.jain@kipi.ai>
2026-05-07 17:48:46 +0000

    added trip_date to trip_facts

--- models/trip_facts.sql
+++ models/trip_facts.sql
@@ -6,7 +6,8 @@ user_type,
 start_station_id, 
 end_station_id, 
 tripduration,
-iff(end_station_id %2 = 0, 'Electric bike', 'Normal Bike') as bike_type
+iff(end_station_id %2 = 0, 'Electric bike', 'Normal Bike') as bike_type, 
+starttime::date as trip_date
  from 
 {{ source('sf_trial_5_may', 'bike_data') }}
 
