1ae32220ac10a59dfd756ab1459cec06379e3314
arinjay jain <arinjay.m.jain@kipi.ai>
2026-05-07 17:29:54 +0000

    Added station_dimesion

---
+++ models/station_dimension.sql
@@ -0,0 +1,8 @@
+Select 
+start_station_id as station_id,
+start_station_name, 
+start_stattion_lat as start_station_lat,
+start_station_lon
+
+    from
+    {{ source('sf_trial_5_may', 'bike_data') }}
