ed29596c38e5111622f745ea305d91cc625f77bb
arinjay jain <arinjay.m.jain@kipi.ai>
2026-05-07 17:44:29 +0000

    Added trip_facts

---
+++ models/trip_facts.sql
@@ -0,0 +1,14 @@
+with trips as 
+(
+Select 
+bikeid, 
+user_type, 
+start_station_id, 
+end_station_id, 
+tripduration,
+iff(end_station_id %2 = 0, 'Electric bike', 'Normal Bike') as bike_type
+ from 
+{{ source('sf_trial_5_may', 'bike_data') }}
+
+)
+select * from trips
